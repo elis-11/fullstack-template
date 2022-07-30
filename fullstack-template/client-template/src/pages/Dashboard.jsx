ba29b9a7f4c14d4537d00c376f94f1502a9650b8
elis-11 <eliza.arzanukaeva@gmail.com>
2022-07-30 18:14:14 +0200

    Protect Dashboard

--- fullstack-template/client-template/src/pages/Dashboard.jsx
+++ fullstack-template/client-template/src/pages/Dashboard.jsx
@@ -1,13 +1,18 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDataContext } from "../context/DataProvider";
 import { getUsersApi } from "../helpers/apiCalls";
-import '../styles/Auth.scss';
+import "../styles/Auth.scss";
 
 export const Dashboard = () => {
+  const navigate = useNavigate ();
+
   const { user, users, setUsers, errors, setErrors } = useDataContext();
 
   useEffect(() => {
-    if (!user) return;
+    if (!user) {
+      return navigate("/login");
+    }
     const loadData = async () => {
       console.log(user);
       const result = await getUsersApi(user.token);
@@ -19,7 +24,7 @@ export const Dashboard = () => {
       setUsers(result);
     };
     console.log(user);
-  
+
     loadData();
   }, [user]);
 
@@ -27,12 +32,12 @@ export const Dashboard = () => {
     <div className="Dashboard">
       <h2>Dashboard</h2>
       <div className="container">
-      {users.map((user) => (
-        <div className="users" key={user._id}>
-          <div>{user.name}</div>
-          <div>{user.email}</div>
-        </div>
-      ))}
+        {users.map((user) => (
+          <div className="users" key={user._id}>
+            <div>{user.name}</div>
+            <div>{user.email}</div>
+          </div>
+        ))}
       </div>
       <div className="errors">{errors}</div>
     </div>
